ddd4fbd2c257436b0a0fa10e5d7011617c81131b
Jose Thomas <jostho@gmail.com>
2020-07-31 21:04:36 +0530

    Add integration test for is_valid_dir_path

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,8 +1,16 @@
+const COUNTRY_FLAGS_DIR: &str = "target/country-flags";
+
+#[test]
+#[ignore]
+fn is_valid_dir_path_for_country_flags() {
+    let result = flock::is_valid_dir_path(COUNTRY_FLAGS_DIR.to_string());
+    assert!(result.is_ok());
+}
+
 #[test]
 #[ignore]
 fn get_countries_for_country_flags() {
-    let flag_dir_path = "target/country-flags";
-    let result = flock::get_countries(flag_dir_path);
+    let result = flock::get_countries(COUNTRY_FLAGS_DIR);
 
     assert!(result.contains_key("AD"));
     assert!(!result.contains_key("AQ"));

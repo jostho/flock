5dea53e0c98621d6889ec8bb5140b0a12d2ed67e
Jose Thomas <jostho@gmail.com>
2020-08-01 18:57:45 +0530

    Update test
    .. after adding MQ to exclusion list

--- tests/integration.rs
+++ tests/integration.rs
@@ -14,5 +14,5 @@ fn get_countries_for_country_flags() {
 
     assert!(result.contains_key("AD"));
     assert!(!result.contains_key("AQ"));
-    assert_eq!(result.len(), 230);
+    assert_eq!(result.len(), 229);
 }
